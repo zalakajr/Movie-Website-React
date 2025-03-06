ee136f6a8d98827e9eaf8d6bf857114c6ead7c4d
zalakajr <zalakajunior12345@gmail.com>
2025-03-06 09:36:09 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
+    <main>
       
-    </div>
+      <div classname="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find <span classname="text-gradient">Movies</span> You'll Enjoy Without the Hassle
+          </h1>
+        </header>
+      </div>
+    </main>
   )
 }
 
